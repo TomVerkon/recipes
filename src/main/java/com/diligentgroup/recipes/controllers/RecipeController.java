f792055412af1643e2f01ec9d388839b3a0e43a9
Tom Verkon <tverkon@usa.net>
2019-01-31 12:54:24 -0500

    Added new recipe capability

--- src/main/java/com/diligentgroup/recipes/controllers/RecipeController.java
+++ src/main/java/com/diligentgroup/recipes/controllers/RecipeController.java
@@ -2,9 +2,13 @@ package com.diligentgroup.recipes.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.diligentgroup.recipes.command.RecipeCommand;
 import com.diligentgroup.recipes.services.RecipeService;
 
 @Controller
@@ -21,6 +25,19 @@ public class RecipeController {
 	public String showRecipeById(@PathVariable String id, Model model) {
 		model.addAttribute("recipe", recipeService.getRecipeById(Long.decode(id)));
 		return "/recipe/show";
+	}
+
+	@RequestMapping("/recipe/new")
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/recipeform";
+	}
+	
+	@PostMapping
+	@RequestMapping("/recipe")
+	public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand) {
+		RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+		return "redirect:/recipe/show/" + savedRecipeCommand.getId();
 	}
 
 }
